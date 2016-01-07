8ad5e45ac2f55b2fa8e133ba6610978ff7c8dd46
ankjai <ankit.jaiswal@appdirect.com>
2016-01-06 17:37:40 -0800

    SFO map working.

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,8 +1,44 @@
-// This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
-function AppViewModel() {
-    this.firstName = "Bert";
-    this.lastName = "Bertington";
+var map;
+var infowindow;
+
+function initMap() {
+    var sfo = {
+        lat: 37.773972,
+        lng: -122.431297
+    };
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: sfo,
+        zoom: 13
+    });
+
+    infowindow = new google.maps.InfoWindow();
+
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+        location: sfo,
+        radius: 500,
+        types: ['store']
+    }, callback);
 }
 
-// Activates knockout.js
-ko.applyBindings(new AppViewModel());
+function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+        }
+    }
+}
+
+function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    });
+}
