4c87ed025b7e0ddcdda1af00aa4a5919beafda5d
ankjai <ankit.jaiswal@appdirect.com>
2016-01-15 15:23:06 -0800

    basic impl of 4square.

--- javascripts/app.js
+++ javascripts/app.js
@@ -11,14 +11,18 @@ function AppViewModel() {
     self.displayList = ko.observableArray();
     self.keyword = ko.observable("");
 
-
     // call to google API
     self.apiCall = function() {
         console.log("apiCall");
         $.ajax({
-                url: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCo449F-wVfaVbN7PG9dG1ygJW2UoJHba0&signed_in=false&libraries=places',
+                url: 'https://maps.googleapis.com/maps/api/js',
                 type: 'GET',
                 dataType: 'jsonp',
+                data: {
+                    key: 'AIzaSyCo449F-wVfaVbN7PG9dG1ygJW2UoJHba0',
+                    signed_in: false,
+                    libraries: 'places'
+                }
             })
             .done(function() {
                 console.log("success");
@@ -64,12 +68,20 @@ function AppViewModel() {
             for (var i = 0; i < self.staticArray.length; i++) {
                 self.createMarker(self.staticArray[i]);
                 self.displayList.push(self.staticArray[i]);
+                self.getPlaceDetails(self.staticArray[i]);
             };
         }
     }
 
     // create map marker
     self.createMarker = function(place) {
+        // console.log(place);
+        // console.log(place.name);
+        // console.log(place.formatted_address);
+        // console.log(place.photos[0].getUrl());
+        // console.log(place.geometry.location.lat());
+        // console.log(place.geometry.location.lng());
+
         var marker = new google.maps.Marker({
             map: self.map,
             position: place.geometry.location
@@ -85,6 +97,40 @@ function AppViewModel() {
             self.infowindow.setContent(place.name);
             self.infowindow.open(self.map, this);
         });
+    }
+
+    // 4square
+    self.getPlaceDetails = function(place) {
+        $.ajax({
+                url: 'https://api.foursquare.com/v2/venues/search',
+                type: 'GET',
+                dataType: 'json',
+                data: {
+                    client_id: 'ZYRAEHGI0G5Z2A3ESQXA0SFT24GW0X00H3MQHZMCWCEHRGO4',
+                    client_secret: 'UTU0XYNCVNFPN1WORMZCNDNLTR4WCWGEETYVBHQS0K3CRYAG',
+                    v: '20160115',
+                    limit: 1,
+                    ll: place.geometry.location.lat() + ',' + place.geometry.location.lng(),
+                    query: place.name
+                },
+            })
+            .done(function(data) {
+                console.log("success");
+                // console.log(data);
+                // console.log("Name:" + data.response.venues[0].name);
+                // console.log("Ph No:" + data.response.venues[0].contact.formattedPhone);
+                // console.log("twitter:" + data.response.venues[0].contact.twitter);
+                // console.log("Addr:" + data.response.venues[0].location.formattedAddress);
+                // console.log("URL:" + data.response.venues[0].url);
+                // console.log("Cusine:" + data.response.venues[0].categories[0].name);
+                // console.log("Menu:" + data.response.venues[0].menu.url);
+            })
+            .fail(function() {
+                console.log("error");
+            })
+            .always(function() {
+                console.log("complete");
+            });
     }
 
     // remove marker
